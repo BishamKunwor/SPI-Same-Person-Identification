9073bc0c7adddef7dd64b7c2f9a642d58e5d4cb9
Bisham Kunwor <novelian.nova@gmail.com>
2023-04-15 23:03:46 +0545

    Added Dummy Db

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,13 +88,78 @@ export default function Home() {
       const newData = { ...data, ...nameChangedData };
       database.push(newData);
     }
-    console.log(database);
-    if (!store.get("createDematAccount")) {
-      store.set("createDematAccount", []);
-    }
+    // if (!store.get("createDematAccount")) {
+    //   store.set("createDematAccount", []);
+    // }
     if (!store.get("usersInfo")) {
       store.set("usersInfo", database);
     }
+    (() => {
+      const data = {
+        accountNo: "1",
+        bankName: "2",
+        branchName: "3",
+        occupation: "1",
+        panNo: "2",
+        natureOfBusiness: "3",
+        organizationName: "4",
+        organizationAddress: "5",
+        designation: "6",
+        employeeId: "7",
+        annualIncome: "8",
+        grandFatherName: "Udaya Bahadur Kunwor",
+        fatherName: "Ramesh Kunwor",
+        motherName: "Junita Kunwor",
+        telephoneNo: "0144166467",
+        mobileNo: "9817843217",
+        email: "bisham.kunwor@smtmcapital.com",
+        country: "Nepal",
+        province: "Bagmati",
+        district: "Sindhuli",
+        gaupalikaNagarpalika: "Kamalamai",
+        wardNo: "6",
+        tole: "Ratamata",
+        houseNo: "05",
+        firstName: "Bisham",
+        lastName: "Kunwor",
+        dateOfBirthBS: "2023-04-01T12:36:03.644Z",
+        dateOfBithAD: "2023-04-02T12:36:05.728Z",
+        educationQualification: "Bachelor",
+        gender: "male",
+        nationality: "male",
+        maritalStatus: "male",
+        citizenshipNo: "1234",
+        issueDistrict: "Sindhuli",
+        issueDate: "2023-04-03T12:36:15.261Z",
+      };
+      const database = [];
+      const names = [
+        {
+          firstName: "Alish",
+          lastName: "Karki",
+        },
+        {
+          firstName: "Sabin",
+          lastName: "Sunuwar",
+        },
+        {
+          firstName: "Ashish",
+          lastName: "Giri",
+        },
+        {
+          firstName: "Anukul",
+          lastName: "Subedi",
+        },
+      ];
+      database.push(data);
+      for (let nameChangedData of names) {
+        const newData = { ...data, ...nameChangedData };
+        database.push(newData);
+      }
+      if (!store.get("createDematAccount")) {
+        store.set("createDematAccount", database);
+      }
+    })();
   }, []);
 
   const kycVerificationLinkRef = useRef<HTMLAnchorElement>(null);
@@ -102,14 +167,14 @@ export default function Home() {
     let style = document.getElementById("afterKycVerification");
     if (style) {
       style.innerText = `.after-content::after{content: '${
-        store.get("createDematAccount")?.length ?? 0
+        store.get("createDematAccount")?.length ?? ""
       }'}`;
       return;
     }
     style = document.createElement("style");
     style.id = "afterKycVerification";
     style.innerText = `.after-content::after{content: '${
-      store.get("createDematAccount")?.length ?? 0
+      store.get("createDematAccount")?.length ?? ""
     }'}`;
 
     document.body.append(style);
